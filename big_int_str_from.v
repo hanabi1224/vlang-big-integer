4598c994456a389952a6aa9645b7d3e6f6f0b148
hanabi1224 <harlowmoo@gmail.com>
2023-07-12 21:43:48 +0800

    fix: change byte to u8

--- big_int_str_from.v
+++ big_int_str_from.v
@@ -7,7 +7,7 @@ pub fn from_str(str string) !BigInteger {
 	char_code_underscore := '_'[0]
 	char_code_comma := ','[0]
 	char_code_minus := '-'[0]
-	mut digits := []byte{}
+	mut digits := []u8{}
 	negative := if str[0] == char_code_minus { true } else { false }
 	digits_str := if negative { str[1..] } else { str }
 	for char_code in digits_str {
